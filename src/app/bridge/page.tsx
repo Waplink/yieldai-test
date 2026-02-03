8c7eebacd3a98cef35efa2643b34ce30f32014f2
waplink <admin@muzu.ru>
2026-02-03 18:07:42 +0300

    3

--- src/app/bridge/page.tsx
+++ src/app/bridge/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+
 import { useState, useEffect, useRef, Suspense, useMemo } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
